875781853a89f29b7e5e162fa25eb26f6ceff225
DESKTOP-JLMBUDS\Dai <nnhhaadd@gmail.com>
2022-04-04 21:39:20 +0700

    fix overview layout, navigation

--- smart_door/apps/home/urls.py
+++ smart_door/apps/home/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('post/ajax/del_sched', views.del_sched, name='del_sched'),
     path('room_image_upload', views.room_image_upload, name='room_image_upload'),
     # The home page
-    path('', views.index, name='home'),
+    path('', views.rooms, name='home'),
     path('<slug:room>',views.room_detail, name='room_detail'),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
